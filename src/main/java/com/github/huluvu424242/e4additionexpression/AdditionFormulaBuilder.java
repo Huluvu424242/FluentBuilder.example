2e244c3f161df3cf173532cf8d7136b1d6b5b385
huluvu424242 <huluvu424242@gmail.com>
2024-01-20 22:50:05 +0100

    refactoring
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- src/main/java/com/github/huluvu424242/e4additionexpression/AdditionFormulaBuilder.java
+++ src/main/java/com/github/huluvu424242/e4additionexpression/AdditionFormulaBuilder.java
@@ -36,7 +36,8 @@ public abstract class AdditionFormulaBuilder implements FormulaStates.Zahl1{
 
     public static FormulaStates.Zahl1 builder() {
         final AdditionFormulaBuilder builder = new AdditionFormulaBuilder() {
-            // will be never called, because is internal overridden by lambda in return of builder() method
+            // will be never called
+            // because is internal overridden by lambda in return of builder() method
             public FormulaContext build() {
                 throw new UnsupportedOperationException("Call of method forbidden");
             }
